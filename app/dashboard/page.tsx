3a3de3b756cdcd66325bc9a3399522756beb5e82
Josue Patricio <josretamozovar@gmail.com>
2024-07-09 22:21:00 -0500

    agregamos rutas protegidas

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,19 @@
 'use client'
 
+import RutaProtegida from '@/components/custom/rutaProtegida'
 import { useAuth } from '@/context/ContextoAuth'
 import React from 'react'
 
 const Dashboard = () => {
 
-  const {usuario}= useAuth()
+  const { usuario } = useAuth()
 
-  console.log("Hola estoy en la vista del dashboard y el usuario es el siguiente",usuario)
+  console.log("Hola estoy en la vista del dashboard y el usuario es el siguiente", usuario)
   return (
-    <div>Soy la vista del dashboard</div>
+    <RutaProtegida>
+      <div>Soy la vista del dashboard</div>
+    </RutaProtegida>
+
   )
 }
 
